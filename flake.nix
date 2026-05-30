8c0d9a586ff2f924fa558d6fc053f70275ad4f66
Tom Herbers <mail@tomherbers.de>
2026-05-30 19:02:42 +0200

    Update to NixOS 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     agenix.url = "github:ryantm/agenix";
   };
 
