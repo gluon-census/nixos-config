7a0a99f7dddc46c8b10ce239fda059bc16692969
Tom Herbers <mail@tomherbers.de>
2026-08-09 02:32:10 +0200

    add comin reboot

--- flake.nix
+++ flake.nix
@@ -10,42 +10,52 @@
     };
   };
 
-  outputs = { self, nixpkgs, agenix, comin, ... }@inputs: {
-    nixosConfigurations.census01 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        agenix.nixosModules.default
-        {
-          environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
-
-          /*system.autoUpgrade = {
-            enable = true;
-            upgrade = false;
-            dates = "hourly";
-            randomizedDelaySec = "15min";
-            persistent = false;
-            flags = [
-              "--print-build-logs"
-              "--update-input" "nixpkgs"
-              "--commit-lock-file"
-              "--recreate-lock-file"
-            ];
-            allowReboot = true;
-          };*/
-        }
-        comin.nixosModules.comin
-        ({
-          services.comin = {
-            enable = true;
-            remotes = [{
-              name = "origin";
-              url = "https://github.com/gluon-census/nixos-config.git";
-              branches.main.name = "main";
-            }];
-          };
-        })
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      agenix,
+      comin,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.census01 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          agenix.nixosModules.default
+          {
+            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
+          }
+          comin.nixosModules.comin
+          ({ pkgs, config, ... }: {
+            services.comin = {
+              enable = true;
+              remotes = [
+                {
+                  name = "origin";
+                  url = "https://github.com/gluon-census/nixos-config.git";
+                  branches.main.name = "main";
+                }
+              ];
+              postDeploymentCommand = "${pkgs.writeShellScriptBin "comin-reboot-if-needed" ''
+                STATUS_JSON=$(${config.services.comin.package}/bin/comin status --json)
+                IS_TESTING=$(echo "$STATUS_JSON" | ${pkgs.jq}/bin/jq '.builder.generation.selected_branch_is_testing')
+                if [ "$IS_TESTING" = "true" ]; then
+                  echo "On a testing branch. Skipping reboot evaluation."
+                  exit 0
+                fi
+                NEED_REBOOT=$(echo "$STATUS_JSON" | ${pkgs.jq}/bin/jq '.need_to_reboot')
+                if [ "$NEED_REBOOT" = "true" ]; then
+                  if ! ${pkgs.coreutils}/bin/sleep 5; then
+                    echo "Warning: sleep failed, proceeding to reboot anyway" >&2
+                  fi
+                  ${pkgs.systemd}/bin/systemctl reboot
+                fi
+              ''}/bin/comin-reboot-if-needed";
+            };
+          })
+        ];
+      };
     };
-  };
 }
